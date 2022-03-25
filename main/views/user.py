22ac9a67d6bb9c3dfbdb8ea47baf83551374ac67
kpastukhov <kirush-ka@yandex.ru>
2022-03-25 14:27:35 +0300

    Implement delete template and workspace for User

--- main/views/user.py
+++ main/views/user.py
@@ -1,7 +1,7 @@
 from aiohttp import web
 from main.models import Template, User, User_Workspace, User_Workspace_Template, Workspace, Workspace_Template
 from psycopg2.errors import UniqueViolation
-from sqlalchemy import insert, select, update
+from sqlalchemy import delete, insert, select, update
 
 
 async def get_all_users(request: web.Request) -> web.json_response:
@@ -14,6 +14,10 @@ async def get_all_users(request: web.Request) -> web.json_response:
 
 async def get_user_by_id(request: web.Request) -> web.json_response:
     user_id = request.match_info["user_id"]
+
+    if not user_id.isdigit():
+        return web.json_response({"status": "fail", "reason": "ID should be an int"}, status=400)
+
     async with request.app["db"].acquire() as conn:
         cursor = await conn.execute(select(User).where(User.id == user_id))
         if cursor.rowcount == 0:
@@ -27,7 +31,7 @@ async def get_user_by_id(request: web.Request) -> web.json_response:
 
 async def create_user(request: web.Request) -> web.json_response:
     data = await request.json()
-    name = data["name"]
+    name = data.get("name")
     if not name:
         return web.json_response({"status": "fail", "reason": "Name can't be empty"}, status=400)
 
@@ -59,9 +63,9 @@ async def get_users_workspaces(request: web.Request) -> web.json_response:
 async def create_new_user_workspace(request: web.Request) -> web.json_response:
     user_id = request.match_info["user_id"]
     data = await request.json()
-    name = data["name"]
+    name = data.get("name")
 
-    if not name and not user_id:
+    if not name or not user_id:
         return web.json_response({"status": "fail", "reason": "Some field is missing"}, status=400)
 
     async with request.app["db"].acquire() as conn:
@@ -114,11 +118,12 @@ async def patch_users_template(request: web.Request) -> web.json_response:
     user_id = request.match_info["user_id"]
     workspace_id = request.match_info["workspace_id"]
     template_id = request.match_info["template_id"]
+
     data = await request.json()
-    config = data["config"]
+    config = data.get("config")
 
     if not config:
-        return web.json_response({"status": "fail", "reason": f"Config field is missing "}, status=400)
+        return web.json_response({"status": "fail", "reason": f"Config field is missing"}, status=400)
 
     async with request.app["db"].acquire() as conn:
         await conn.execute(
@@ -139,6 +144,48 @@ async def patch_users_template(request: web.Request) -> web.json_response:
         )
         updated_template = await cursor.fetchone()
         return web.json_response({"status": "ok", "data": dict(updated_template)}, status=200)
+
+
+async def delete_users_template(request: web.Request) -> web.json_response:
+    user_id = request.match_info["user_id"]
+    workspace_id = request.match_info["workspace_id"]
+    template_id = request.match_info["template_id"]
+
+    async with request.app["db"].acquire() as conn:
+        cursor = await conn.execute(
+            delete(User_Workspace_Template).where(
+                User_Workspace_Template.template_id == template_id,
+                User_Workspace_Template.user_id == user_id,
+                User_Workspace_Template.workspace_id == workspace_id,
+            )
+        )
+        if cursor.rowcount == 1:
+            return web.json_response({"status": "ok", "data": []}, status=200)
+        return web.json_response({"status": "fail", "reason": f"Template {template_id} doesn't exist"}, status=404)
+
+
+async def delete_users_workspace(request: web.Request) -> web.json_response:
+    user_id = request.match_info["user_id"]
+    workspace_id = request.match_info["workspace_id"]
+
+    async with request.app["db"].acquire() as conn:
+        cursor = await conn.execute(select(User_Workspace))
+        all_workspaces = await cursor.fetchall()
+        all_workspaces = [dict(q) for q in all_workspaces]
+
+        user_workspaces = [x.get("workspace_id") for x in all_workspaces if x.get("user_id") == int(user_id)]
+
+        if int(workspace_id) not in user_workspaces:
+            return web.json_response(
+                {"status": "fail", "reason": f"Workspace {workspace_id} doesn't does not belong to user {user_id}"},
+                status=400,
+            )
+
+        cursor = await conn.execute(delete(Workspace).where(Workspace.id == workspace_id))
+
+        if cursor.rowcount == 1:
+            return web.json_response({"status": "ok", "data": []}, status=200)
+        return web.json_response({"status": "fail", "reason": f"Workspace {workspace_id} doesn't exist"}, status=404)
 
 
 async def user_new_template(request: web.Request) -> web.json_response:
