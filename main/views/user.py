3e1cce8bb89e904b73a7d5e955889fea39f3b0a1
Kirill Pastukhov <kirush-ka@yandex.ru>
2022-04-01 18:04:21 +0300

    User crud finish

--- main/views/user.py
+++ main/views/user.py
@@ -32,6 +32,10 @@ async def get_user_by_id(request: web.Request) -> web.json_response:
 
 async def update_user_by_id(request: web.Request) -> web.json_response:
     user_id = request.match_info["user_id"]
+
+    if not user_id.isdigit():
+        return web.json_response({"status": "fail", "reason": "User id should be an int"}, status=400)
+
     data = await vaildate_body(request)
     if not data or isinstance(data, list):
         return web.json_response({"status": "fail", "reason": "Invalid body"}, status=400)
@@ -102,9 +106,9 @@ async def create_user_workspace(request: web.Request) -> web.json_response:
     if not data or isinstance(data, list):
         return web.json_response({"status": "fail", "reason": "Invalid body"}, status=400)
     name = data.get("name")
-    type = data.get("type")
+    workspace_type = data.get("type")
 
-    if not name or not user_id or not type:
+    if not name or not user_id or not workspace_type:
         return web.json_response({"status": "fail", "reason": "Some field is missing"}, status=400)
 
     async with request.app["db"].acquire() as conn:
@@ -112,11 +116,9 @@ async def create_user_workspace(request: web.Request) -> web.json_response:
             cursor = await conn.execute(insert(Workspace).values(name=name))
             new_user_workspace = await cursor.fetchone()
 
-            cursor = await conn.execute(
-                insert(User_Workspace).values(user_id=user_id, workspace_id=new_user_workspace.id)
-            )
+            cursor = await conn.execute(insert(User_Workspace).values(user_id=user_id, workspace_id=new_user_workspace.id))
 
-            cursor = await conn.execute(select(Workspace_Template).join(Workspace).where(Workspace.type == type))
+            cursor = await conn.execute(select(Workspace_Template).join(Workspace).where(Workspace.type == workspace_type))
             all_templates = await cursor.fetchall()
 
             template_ids = [x.template_id for x in all_templates]
@@ -135,9 +137,7 @@ async def create_user_workspace(request: web.Request) -> web.json_response:
 
             return web.json_response({"status": "ok", "data": dict(new_user_workspace)}, status=201)
         except UniqueViolation:
-            return web.json_response(
-                {"status": "fail", "reason": "Workspace with such name already exists"}, status=400
-            )
+            return web.json_response({"status": "fail", "reason": "Workspace with such name already exists"}, status=400)
 
 
 async def get_users_templates_for_workspace(request: web.Request) -> web.json_response:
@@ -145,9 +145,7 @@ async def get_users_templates_for_workspace(request: web.Request) -> web.json_re
     workspace_id = request.match_info["workspace_id"]
     async with request.app["db"].acquire() as conn:
         cursor = await conn.execute(
-            select(User_Workspace_Template).where(
-                User_Workspace_Template.workspace_id == workspace_id, User_Workspace_Template.user_id == user_id
-            )
+            select(User_Workspace_Template).where(User_Workspace_Template.workspace_id == workspace_id, User_Workspace_Template.user_id == user_id)
         )
         if cursor.rowcount == 0:
             return web.json_response(
@@ -253,9 +251,7 @@ async def create_user_template(request: web.Request) -> web.json_response:
             cursor = await conn.execute(insert(Template).values(name=template_name, config=config))
             new_user_template = await cursor.fetchone()
             cursor = await conn.execute(
-                insert(User_Workspace_Template).values(
-                    user_id=user_id, workspace_id=workspace_id, template_id=new_user_template.id, config=config
-                )
+                insert(User_Workspace_Template).values(user_id=user_id, workspace_id=workspace_id, template_id=new_user_template.id, config=config)
             )
             return web.json_response({"status": "ok", "data": dict(new_user_template)}, status=201)
         except UniqueViolation:
